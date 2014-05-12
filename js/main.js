a06d6095bd6a24ab3a8bdf5af1438b8fe081fb75
Evelyn Rios <everios@me.com>
2014-05-12 08:12:02 -0700

    Added additional logic.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,49 @@
 $(document).ready(function(){
 	$('.submit').click(function(e){
 		e.preventDefault();
-		var fourPM = {};
-		var fivePM = {};
-		var sixPM = {};
+		var fourPM = [];
+		var fivePM = [];
+		var sixPM = [];
 		$('.form-group').each(function(){
-			if ($(this).find('[type=checkbox]').is(':checked')) {
-				var $RButtons = $(this).find('.exclusivity');
-				var tempArray = new Array();
-				var i = 0;
-				jQuery.each($RButtons, function () {
-					if ($(this).is(':checked')) {
-						var value = $(this).attr('value');
-						tempArray[i] = value;
-						i++;
-					}
-				});
-				var reporter = $(this).find('.reporter').val();
-				var slug = $(this).find('.slug').val();
-				if (tempArray == "4only") {
-					fourPM[reporter] = slug;					
-				} else if (tempArray == "5only") {
-					fivePM[reporter] = slug;		
-				} else if (tempArray == "6only") {
-					sixPM[reporter] = slug;	
+			var reporter = $(this).find('.reporter').val();
+			var slug = $(this).find('.slug').val();
+			var tempObject = {};
+			tempObject[reporter] = slug;
+			var lede = $(this).find('[type=checkbox]').is(':checked');
+			tempObject["lede"] = lede;
+			var $RButtons = $(this).find('.exclusivity');
+			var tempArray = new Array();
+			var i = 0;
+			jQuery.each($RButtons, function () {
+				if ($(this).is(':checked')) {
+					var value = $(this).attr('value');
+					tempArray[i] = value;
+					i++;
+				}
+			});
+			if (tempArray == "4only") {
+				fourPM.push(tempObject);					
+			} else if (tempArray == "5only") {
+				fivePM.push(tempObject);		
+			} else if (tempArray == "6only") {
+				sixPM.push(tempObject);
+			} else {
+				var computerChoice = Math.random();
+				if (computerChoice < 0.34) {
+					fourPM.push(tempObject);
+				} else if (computerChoice <= 0.67) {
+					fivePM.push(tempObject);	
 				} else {
-					var computerChoice = Math.random();
-					console.log(computerChoice);
-					if (computerChoice < 0.34) {
-						fourPM[reporter] = slug;
-					} else if (computerChoice <= 0.67) {
-						fivePM[reporter] = slug;	
-					} else {
-						sixPM[reporter] = slug;
-					}
+					sixPM.push(tempObject);
 				}
 			}
 		});
+		function sorter(obj1, obj2) {
+			return obj2.lede - obj1.lede;
+		}
+		fourPM.sort(sorter);
+		fivePM.sort(sorter);
+		sixPM.sort(sorter);
 		console.log(fourPM);
 		console.log(fivePM);
 		console.log(sixPM);
