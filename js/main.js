77fcc2de0609d1497938288f869adc42eba02b75
Evelyn Rios <everios@me.com>
2014-05-13 08:30:06 -0700

    Bug fixes to output & date added to modal.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	$('.submit').click(function(e){
 		e.preventDefault();
+
+		var d = new Date();
+		var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+		console.log(document.getElementById("modalLabel").innerHTML);
+		document.getElementById("modalLabel").innerHTML = days[d.getDay()] + "'s Split:";
+
 		var fourPM = [];
 		var fivePM = [];
 		var sixPM = [];
@@ -8,7 +14,8 @@ $(document).ready(function(){
 			var reporter = $(this).find('.reporter').val();
 			var slug = $(this).find('.slug').val();
 			var tempObject = {};
-			tempObject[reporter] = slug;
+			tempObject["reporter"] = reporter;
+			tempObject["slug"] = slug;
 			var lede = $(this).find('[type=checkbox]').is(':checked');
 			tempObject["lede"] = lede;
 			var $RButtons = $(this).find('.exclusivity');
@@ -44,8 +51,30 @@ $(document).ready(function(){
 		fourPM.sort(sorter);
 		fivePM.sort(sorter);
 		sixPM.sort(sorter);
-		console.log(fourPM);
-		console.log(fivePM);
-		console.log(sixPM);
+		// console.log(fourPM);
+		// console.log(fivePM);
+		// console.log(sixPM);
+		function createDiv(obj){
+			var storyName = obj.slug;
+			var reporterName = obj.reporter;
+			var contentString = '<div>' + reporterName + ' - ' + storyName + '</div>';
+			var div = document.createElement('div');
+			div.innerHTML = contentString;
+			return div;
+		}
+		var arr = [fourPM, fivePM, sixPM];
+		jQuery.each(arr, function(index, value){
+			for (var i = 0; i < value.length; i++) {
+				var createdDiv = createDiv(value[i]);
+				if (index == 0){
+					$('.4PMmodal').append(createdDiv);
+				} else if (index == 1){
+					$('.5PMmodal').append(createdDiv);
+				} else {
+					$('.6PMmodal').append(createdDiv);
+				}
+			}
+		});
+
 	});
 });
