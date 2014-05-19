d1e4001d7ebd63be186eaf061b796a302cee38d6
Evelyn Rios <everios@me.com>
2014-05-18 21:06:45 -0700

    Added logic for 2hits.

--- js/main.js
+++ js/main.js
@@ -13,7 +13,9 @@ $(document).ready(function(){
 		var fourPM = [];
 		var fivePM = [];
 		var sixPM = [];
+
 		$('.form-group').each(function(){
+
 			var reporter = $(this).find('.reporter').val();
 			var slug = $(this).find('.slug').val();
 			var tempObject = {};
@@ -27,6 +29,7 @@ $(document).ready(function(){
 
 			var hitsRadios = $(this).find('.hits');
 			var hitsValue;
+
 			jQuery.each(hitsRadios, function () {
 				if ($(this).is(':checked')) {
 					hitsValue = $(this).attr('value');
@@ -38,6 +41,19 @@ $(document).ready(function(){
 					exclusivityValue = $(this).attr('value');
 				}
 			});
+
+			function computerChooses() {
+				var computerChoice = Math.random();
+				if (computerChoice < 0.34) {
+					fourPM.push(tempObject);
+				} else if (computerChoice <= 0.67) {
+					fivePM.push(tempObject);	
+				} else {
+					sixPM.push(tempObject);
+				}
+				return computerChoice;
+			}
+
 			if (exclusivityValue == "4only") {
 				fourPM.push(tempObject);					
 			} else if (exclusivityValue == "5only") {
@@ -48,57 +64,70 @@ $(document).ready(function(){
 				fourPM.push(tempObject);
 				fivePM.push(tempObject);
 				sixPM.push(tempObject);
-			} else if (hitsValue == "2hits") {
-
-			} else {
-				var computerChoice = Math.random();
-				if (computerChoice < 0.34) {
-					fourPM.push(tempObject);
-				} else if (computerChoice <= 0.67) {
-					fivePM.push(tempObject);	
+			} else if (hitsValue == "2hits") {				
+				var firstResult = computerChooses();
+				var secondResult = Math.random();
+				if (firstResult < 0.34) {
+					if (secondResult < 0.5) {
+						fivePM.push(tempObject);
+					} else {
+						sixPM.push(tempObject);
+					}
+				} else if (firstResult <= 0.67) {
+					if (secondResult < 0.5) {
+						fourPM.push(tempObject);
+					} else {
+						sixPM.push(tempObject);
+					}
 				} else {
-					sixPM.push(tempObject);
+					if (secondResult < 0.5) {
+						fourPM.push(tempObject);
+					} else {
+						fivePM.push(tempObject);
+					}
 				}
+			} else {
+				computerChooses();
 			}
 		});
 
-	function sorter(obj1, obj2) {
-		return obj2.lede - obj1.lede;
-	}
-	fourPM.sort(sorter);
-	fivePM.sort(sorter);
-	sixPM.sort(sorter);
+function sorter(obj1, obj2) {
+	return obj2.lede - obj1.lede;
+}
+fourPM.sort(sorter);
+fivePM.sort(sorter);
+sixPM.sort(sorter);
 
-	function createDiv(obj){
-		var storyName = obj.slug;
-		var reporterName = obj.reporter;
-		var template = Handlebars.compile($("#data-template").html());
-		var context = {reporterName: reporterName, storyName: storyName}
-		var html    = template(context);
-		return html;
-	}
-	var arr = [fourPM, fivePM, sixPM];
-	jQuery.each(arr, function(index, value){
-		for (var i = 0; i < value.length; i++) {
-			var createdDiv = createDiv(value[i]);
-			if (index == 0){
-				$('.4PMmodal').append(createdDiv);
+function createDiv(obj){
+	var storyName = obj.slug;
+	var reporterName = obj.reporter;
+	var template = Handlebars.compile($("#data-template").html());
+	var context = {reporterName: reporterName, storyName: storyName}
+	var html    = template(context);
+	return html;
+}
+var arr = [fourPM, fivePM, sixPM];
+jQuery.each(arr, function(index, value){
+	for (var i = 0; i < value.length; i++) {
+		var createdDiv = createDiv(value[i]);
+		if (index == 0){
+			$('.4PMmodal').append(createdDiv);
 		} else if (index == 1){
 			$('.5PMmodal').append(createdDiv);
 		} else {
 			$('.6PMmodal').append(createdDiv);
 		}
 	}
-	});
+});
 
-	$(function () {
-		$(".grid").sortable({
-			tolerance: 'pointer',
-			revert: 'invalid',
-			connectWith: ".connectedSortable",
-			forceHelperSize: true
-		});
+$(function () {
+	$(".grid").sortable({
+		tolerance: 'pointer',
+		revert: 'invalid',
+		connectWith: ".connectedSortable",
+		forceHelperSize: true
 	});
+});
 
-	});
+});
 });
